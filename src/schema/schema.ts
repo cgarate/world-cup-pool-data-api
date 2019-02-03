bf0377a6ba74ed6c287a40da260a99c3a0d650cc
Carlos Garate <carlos.m.garate@gmail.com>
2019-02-03 15:29:40 -0500

    Adds mock data

--- src/schema/schema.ts
+++ src/schema/schema.ts
@@ -9,8 +9,29 @@ const {
   GraphQLID,
   GraphQLInt,
   GraphQLList,
-  GraphQLNonNull,
+  GraphQLNonNull
 } = graphql;
+
+const teams = [
+  {
+    emoji: "flag-mx",
+    fifaCode: "MEX",
+    flag:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/Flag_of_Mexico.svg/800px-Flag_of_Mexico.png",
+    id: 22,
+    iso2: "mx",
+    name: "Mexico",
+  },
+  {
+    emoji: "flag-ar",
+    fifaCode: "ARG",
+    flag:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/Flag_of_Argentina.svg/800px-Flag_of_Argentina.png",
+    id: 13,
+    iso2: "ar",
+    name: "Argentina",
+  },
+];
 
 const TeamType = new GraphQLObjectType({
   fields: () => ({
@@ -19,7 +40,7 @@ const TeamType = new GraphQLObjectType({
     flag: { type: GraphQLString },
     id: { type: GraphQLID },
     iso2: { type: GraphQLString },
-    name: { type: GraphQLString },
+    name: { type: GraphQLString }
   }),
-  name: "Team",
+  name: "Team"
 });
